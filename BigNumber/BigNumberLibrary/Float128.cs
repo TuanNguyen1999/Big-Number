f89bf0202eed027df4485f7786cd2c1606fefb74
tuan nguyen <proplaydota12345@gmail.com>
2019-03-29 22:25:25 +0700

    Implement BitArray class
    BitArray is used for creating, manipulating a serie of bits.
    This class is used as base class for other numeric types

---
+++ BigNumber/BigNumberLibrary/Float128.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNumberLibrary
+{
+    public class Float128 : BitArray
+    {
+        new const uint TotalBits = 128u;
+        new const uint TotalBytes = 16u;
+        public Float128() : base(TotalBits) { }
+
+        /// <summary>
+        /// Create an instance with all bit set to 0
+        /// </summary>
+        /// <returns>BitArray instance with all bit set to 0</returns>
+        public static Float128 AllZero()
+        {
+            return AllZero(TotalBits) as Float128;
+        }
+
+        /// <summary>
+        /// Create an instance with all bit set to 1
+        /// </summary>
+        /// <returns>BitArray instance with all bit set to 1</returns>
+        public static Float128 AllOne()
+        {
+            return AllOne(TotalBits) as Float128;
+        }
+    }
+
+
+}
